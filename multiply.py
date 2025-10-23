e8b9e78150f325ffc6477d7b25fad379a2c6785b
Laurine_Njeru <njerulaurine8@gmail.com>
2025-10-22 23:57:40 -0700

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,6 @@
+#product of two numbers from user input
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+product=num1*num2
+print("The product of",num1,"and",num2,"is",product)
+
